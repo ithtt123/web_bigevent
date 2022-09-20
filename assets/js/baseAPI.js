0dc02d8594290f7b09e9103e6575342a55f24b58
ithtt <ithtt@itcast.cn>
2022-09-20 14:31:27 +0800

    完成了主页开发功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,23 @@
-$.ajaxPrefilter(function(options){
-  options.url ='http://www.liulongbin.top:3007'+options.url
-  console.log(options.url)
+$.ajaxPrefilter(function (options) {
+  // 统一设置根路径
+  options.url = 'http://www.liulongbin.top:3007' + options.url
+  // console.log(options.url)
+  // 统一设置headers请求头
+  if (options.url.indexOf('/my/') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+
+  // 全局统一挂载complete无论成功还是失败都会调用这个函数
+  options.complete = function (res) {
+    console.log(res)
+    // responseJSON可以拿到服务器响应的数据
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      // 强制清空本地存储toktn
+      localStorage.removeItem('token')
+      // 强制跳转页面到login
+      location.href = 'login.html'
+    }
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,55 @@
+$(function () {
+  // 调用函数，获取基本信息
+  getUserInfo()
+  let layer = layui.layer
+
+  // 点击按钮实现退出功能
+  $('#btnLogoot').on('click', function () {
+    console.log('ok')
+    layer.confirm('确认退出登录', { icon: 3, title: '提示' },
+      function (index) {
+        //do something
+        // 清空本地存储toktn
+        localStorage.removeItem('token')
+        // 跳转页面到login
+        location.href = 'login.html'
+
+        layer.close(index);
+      });
+  })
+})
+
+function getUserInfo() {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    // headers是请求头配置对象
+    // headers: {
+    //   Authorization: localStorage.getItem('token') || ''
+    // },
+    success(res) {
+      if (res.status !== 0) {
+        return layui.layer.msg('获取信息失败')
+      }
+      // console.log(res)
+      // 调用renderAvatar渲染用户头像
+      renderAvatar(res.data)
+    }
+  })
+}
+
+// 渲染用户头像
+function renderAvatar(user) {
+  // 获取用户名称
+  let name = user.nickname || user.username
+  $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+  // 渲染用户头像
+  if (user.user_pic !== null) {
+    $('.layui-nav-img').attr('scr', user.user_pic).show()
+    $('.text-avatar').hide()
+  } else {
+    $('.layui-nav-img').hide()
+    let first = name[0].toUpperCase()
+    $('.text-avatar').html(first).show()
+  }
+}
